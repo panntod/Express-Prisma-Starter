eb4286ee98f9561fd4bb0fa545e58718428a066b
panntod <pandhu.munjalindra@gmail.com>
2024-05-25 05:46:53 +0700

    feat: Create api response template

---
+++ src/utils/apiResponse.ts
@@ -0,0 +1,42 @@
+interface MessageResponse {
+  message: string;
+  status: boolean;
+  [key: string]: unknown;
+}
+
+export function Success(
+  message: string,
+  otherResponses?: Record<string, unknown>
+): MessageResponse {
+  return { message, status: true, ...otherResponses };
+}
+
+export function CreatedSuccessfully(
+  message: string,
+  otherResponses?: Record<string, unknown>
+): MessageResponse {
+  return { status: true, message, success: true, ...otherResponses };
+}
+
+export function InternalServerError(message?: string): MessageResponse {
+  return {
+    message: message || "Something Went Wrong",
+    status: false,
+  };
+}
+
+export function Forbidden(message: string): MessageResponse {
+  return { message, status: false };
+}
+
+export function Unauthorize(message: string): MessageResponse {
+  return { message, status: false };
+}
+
+export function BadRequest(message: string): MessageResponse {
+  return { message, status: false };
+}
+
+export function NotFound(message: string): MessageResponse {
+  return { message, status: false };
+}
